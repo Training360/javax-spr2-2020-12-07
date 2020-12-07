955dc2657f192c376946275ee0543d1ecc1765ea
iviczian <viczian.istvan@gmail.com>
2020-12-07 13:52:43 +0100

    Core

---
+++ employees-core/src/main/java/training/employees/EmployeesApplication.java
@@ -0,0 +1,21 @@
+package training.employees;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+public class EmployeesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeesApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
+
+}
