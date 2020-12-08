969ef7b6e9cc4918613d33a887da26cb9a2d6a38
iviczian <viczian.istvan@gmail.com>
2020-12-08 13:37:11 +0100

    Event dobás service rétegből

--- employees-websocket/src/main/java/training/employees/StompController.java
+++ employees-websocket/src/main/java/training/employees/StompController.java
@@ -1,17 +1,28 @@
 package training.employees;
 
+import lombok.AllArgsConstructor;
+import org.springframework.context.event.EventListener;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 import java.time.LocalDateTime;
 
 @Controller
+@AllArgsConstructor
 public class StompController {
+
+    private final SimpMessagingTemplate template;
 
     @MessageMapping("/messages")
     @SendTo("/topic/employees")
     public Message handleMessage(MessageCommand messageCommand) {
         return new Message("Message from server: " + messageCommand.getContent() + " " + LocalDateTime.now());
+    }
+
+    @EventListener
+    public void handleEvent(EmployeeHasCreatedEvent event) {
+        template.convertAndSend("/topic/employees", new Message("Employee has created: " + event.getName()));
     }
 }
