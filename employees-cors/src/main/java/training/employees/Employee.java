6f4d0117c460718100b3a6eacb3f0e1352a0b140
iviczian <viczian.istvan@gmail.com>
2020-12-09 10:03:21 +0100

    CORS kiindulas

---
+++ employees-cors/src/main/java/training/employees/Employee.java
@@ -0,0 +1,25 @@
+package training.employees;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "emp_name")
+    private String name;
+
+    public Employee(String name) {
+        this.name = name;
+    }
+}
